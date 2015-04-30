2f9ca5c139490a1f80cdd6e5d2fd43c3de94c9fc
Donald Linnemeyer <dlinnemeyer@economicmodeling.com>
2015-04-30 14:04:46 -0700

    adding interactive mode and a -f flag to build.sh

--- build.sh
+++ build.sh
@@ -1,15 +1,32 @@
 #!/bin/bash
 
+# interactive mode on copy unless they want to force the update
+if [ ! "$1" == "-f" ]; then
+	i="-i"
+fi
+
 echo "copying .vimrc and .vim/"
-cp -i .vimrc ~/.vimrc
-cp -ir .vim/ ~/.vim/
+cp $i .vimrc ~/.vimrc
+if [ ! -d ~/.vim ]; then
+    mkdir ~/.vim && cp -r .vim/* ~/.vim/
+else
+	# don't use native cp -i here, since there are so many individual files
+	if [ "$i" == "-i" ]; then
+		echo -n "overwrite $HOME/.vim? "
+	    read response
+	else 
+		response="y"
+	fi
+
+	[[ "$response" == "y" || "$response" == "yes" ]] && cp -r .vim/* ~/.vim/
+fi
 
 echo "copying .bash_profile"
-cp -i .bash_profile ~/.bash_profile
+cp $i .bash_profile ~/.bash_profile
 
 echo "copying .gitconfig"
-cp -i .gitconfig ~/.gitconfig
+cp $i .gitconfig ~/.gitconfig
 
 echo "copying .i3/"
 [ ! -d ~/.i3 ] && mkdir ~/.i3
-cp -i .i3/* ~/.i3
+cp $i .i3/* ~/.i3
